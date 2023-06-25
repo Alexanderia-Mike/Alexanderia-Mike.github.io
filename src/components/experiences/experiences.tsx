7a5aa9650598e74190a31ad77e486f1b05d3777d
Alexanderia-Mike <cf_lou@sjtu.edu.cn>
2023-06-24 17:08:42 -0700

    completed experiences part

--- src/components/experiences/experiences.tsx
+++ src/components/experiences/experiences.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionHeader from "../common/sectionHeader";
+import ExperiencesItem from "./experiencesItem";
 
 interface Props {
     // TODO
@@ -13,8 +14,44 @@ class Experiences extends React.Component<Props> {
                     imageUrl="images/experiences-header.png"
                 ></SectionHeader>
                 {/* C9BEB7 */}
-                <div className="pt-4">
-                    {/* // TODO */}
+                <div className="pt-4 row justify-content-center">
+                    <ExperiencesItem
+                        imageUrl='images/amazon.jpeg'
+                        title='Intern Software Development Engineer'
+                        time='May 2023 - Aug 2023'
+                        location='Amazon.com Inc, Seattle, WA'
+                        descriptions={[
+                            'Directed the design of backend database structures and GraphQL queries for Homepage Outfit Recommendation widget.',
+                            'Implemented frontend user interface (Typescript) of the widget.',
+                            'Led A/B testing on different interactive modes of the widget.'
+                        ]}
+                        link='https://www.aboutamazon.com/'
+                        linkText='Amazon.com Inc Website'
+                    ></ExperiencesItem>
+                    <ExperiencesItem
+                        imageUrl='images/bianjie.png'
+                        title='Intern Federated Learning Algorithm Engineer'
+                        time='May 2021 - Aug 2021'
+                        location='Bianjie.AI, Shanghai, China'
+                        descriptions={[
+                            'Initiated research projects upon federated learning algorithm for 4 model types and hundreds of hyperparameter settings.',
+                            'Created a federated learning SDK (Python) as a core module in company\'s final product.'
+                        ]}
+                        link='https://www.bianjie.ai/en/'
+                        linkText='Bianjie.AI Company Website'
+                    ></ExperiencesItem>
+                    <ExperiencesItem
+                        imageUrl='images/smec.jpg'
+                        title='Intern IGBT Department Engineer'
+                        time='Aug 2020 - Sept 2020'
+                        location='SMEC, Shaoxing, Zhejiang, China'
+                        descriptions={[
+                            'Inspected the surface defects of wafers and verified the possible reasons based on the collected data and over 10 related academic essays.',
+                            'delivered a 20-minute presentation proposing suggestions of surface defects, which brought 20% improvements on wafers yield.'
+                        ]}
+                        link='https://www.smecs.com/'
+                        linkText='SMEC Company Website'
+                    ></ExperiencesItem>
                 </div>
             </div>
         );
--- src/components/experiences/experiencesItem.tsx
+++ src/components/experiences/experiencesItem.tsx
@@ -1,11 +1,55 @@
 import React from "react";
 
 interface Props {
-
-};
+    imageUrl: string;
+    title: string;
+    time: string;
+    location: string;
+    descriptions: string[];
+    link?: string;
+    linkText?: string;
+}
 
 class ExperiencesItem extends React.Component<Props> {
-    
+    private putBullet(bullet: string): React.ReactNode {
+        return (
+            <li className="mb-1">{bullet}</li>
+        );
+    }
+
+    render(): React.ReactNode {
+        let linkText = this.props.linkText || 'company website';
+        let link: React.ReactNode
+        if (this.props.link)
+            link = (
+                <p>
+                    <a href={this.props.link} target="_blank" className="gap-1 btn btn-secondary mt-2">
+                        {linkText}
+                    </a>
+                </p>
+            );
+        else
+            link = <></>;
+        
+        return (
+            <div className="col-10 col-md-9">
+                <div className="row g-0 flex-md-row mb-4 shadow-sm">
+                    <a href={this.props.link || '#'} target="_blank" className="col-auto d-none d-xl-block">
+                        <img src={this.props.imageUrl} height={300} width={300}/>
+                    </a>
+                    <div className="col overflow-auto py-3 ps-4 pe-2 d-flex flex-column" style={{height: "300px"}}>
+                        <strong className="d-inline-block mb-2 text-primary-emphasis">{this.props.location}</strong>
+                        <h3 className="mb-0">{this.props.title}</h3>
+                        <div className="mb-1 text-body-secondary">{this.props.time}</div>
+                        <ul className="card-text mb-auto">
+                            {this.props.descriptions.map((bullet, i) => this.putBullet(bullet))}
+                        </ul>
+                        {link}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ExperiencesItem;
