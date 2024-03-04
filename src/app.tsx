98bfa48614fddd5ca1aaea8d01e67eaf8523d712
Alexanderia-Mike <chenfeil@andrew.cmu.edu>
2024-03-03 21:18:44 -0500

    made header page responsive to scroll

--- src/app.tsx
+++ src/app.tsx
@@ -10,29 +10,72 @@ import Experiences from './components/experiences/experiences';
 import Research from './components/research/research';
 import Contact from './components/contact/contact';
 import PhoneticIcon from './components/phonetic/phoneticIcon';
+import { InView } from 'react-intersection-observer';
 
 interface Props {
 }
 
-class App extends React.Component<Props> {
+interface States {
+    in_views: boolean[];
+}
+
+class App extends React.Component<Props, States> {
+    private static readonly defaultNavItemName: string = 'Home';
+    private static navItemList: string[] = [
+        App.defaultNavItemName, 
+        'Education', 
+        'Projects', 
+        'Experiences', 
+        'Researches', 
+        'Contact'
+    ];
 
     constructor(props: Props) {
         super(props);
+        this.state ={
+            in_views: App.navItemList.map(_ => false)
+        };
     }
     
     componentDidMount(): void {}
 
+    scrollChangeHandlerFactory(idx: number) {
+        return (in_view: boolean) => {
+            let in_views = this.state.in_views;
+            in_views[idx] = in_view;
+            this.setState({
+                ...this.state,
+                in_views: in_views
+            });
+        };
+    }
+
     render(): React.ReactNode {
-        return (
+        let elements = [
             <div>
-                <Header></Header>
                 <Profile></Profile>
                 <SelfIntro></SelfIntro>
-                <Education></Education>
-                <Projects></Projects>
-                <Experiences></Experiences>
-                <Research></Research>
-                <Contact></Contact>
+            </div>,
+            <Education></Education>,
+            <Projects></Projects>,
+            <Experiences></Experiences>,
+            <Research></Research>,
+            <Contact></Contact>
+        ]
+        return (
+            <div>
+                <Header
+                    nav_items={App.navItemList}
+                    in_views={this.state.in_views}
+                ></Header>,
+                {elements.map((elmt, idx) => (
+                    <InView
+                        onChange={this.scrollChangeHandlerFactory(idx)}
+                        threshold={0.2}
+                    >
+                        {elmt}
+                    </InView>
+                ))}
                 <Footer></Footer>
                 <PhoneticIcon></PhoneticIcon>
             </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { InView } from "react-intersection-observer";
 import "./header.css";
 
 interface NavItemProps {
@@ -8,11 +9,8 @@ interface NavItemProps {
 };
 
 interface Props {
-    // TODO
-};
-
-interface State {
-    activeNavItem: string;
+    nav_items: string[];
+    in_views: boolean[];
 };
 
 class NavItem extends React.Component<NavItemProps> {
@@ -36,48 +34,32 @@ class NavItem extends React.Component<NavItemProps> {
     }
 }
 
-class Header extends React.Component<Props, State> {
-    private static readonly defaultNavItemName: string = 'Home';
-
+class Header extends React.Component<Props> {
     constructor(props: Props) {
         super(props);
-        this.state = {
-            activeNavItem: Header.defaultNavItemName
-        };
     }
 
-    createNavItem(itemName: string, activeNavItem: string, clickHandler: React.MouseEventHandler): React.ReactNode {
+    createNavItem(itemName: string, is_active: boolean, clickHandler: React.MouseEventHandler): React.ReactNode {
         return (
             <NavItem
-                active={itemName == activeNavItem}
+                active={is_active}
                 text={itemName}
                 clickHandler={clickHandler}
             />
         );
     }
 
-    private clickHandlerGenerator = (itemName: string) => {
-        let clickHandler = (e: React.MouseEvent) => {
-            // e.preventDefault();
-            this.setState({
-                activeNavItem: itemName
-            });
-        }
-        return clickHandler;
-    }
-
     render(): React.ReactNode {
-        let navItemList: string[] = [Header.defaultNavItemName, 'Education', 'Projects', 'Experiences', 'Researches', 'Contact'];
         return (
-            <div className="container-xxl col-10 mb-5" style={{backgroundColor: "#D7D6D100"}}>
+            <div className="container-xxl sticky-top col-12 mb-5" style={{backgroundColor: "#F1F1F1"}}>
                 <header className="d-flex flex-wrap flex-wrap justify-content-center py-3 mb-4">
                     <a href="index.html" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                         <img className="bi me-2" height="60" src={'images/logo.png'}/>
                     </a>
 
                     <ul className="nav nav-pills">
-                        {navItemList.map((itemName, i) => 
-                            this.createNavItem(itemName, this.state.activeNavItem, this.clickHandlerGenerator(itemName)))}
+                        {this.props.nav_items.map((itemName, i) => 
+                            this.createNavItem(itemName, this.props.in_views[i], () => {}))}
                     </ul>
                 </header>
             </div>
